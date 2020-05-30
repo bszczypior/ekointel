d5e8b889c373e6d17ee75bf801ed9ca389eb4372
bartek <bartlomiejszczypior@gmail.com>
2020-05-30 15:33:11 +0200

    Updates

---
+++ precache-manifest.e9a4c900d14e1806474acda0dfb801b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8782f2ac49a6fa7e06da980d1cb1e78",
+    "url": "/ekointel/index.html"
+  },
+  {
+    "revision": "9ac2f1189d13f3f5c12d",
+    "url": "/ekointel/static/css/main.3828133f.chunk.css"
+  },
+  {
+    "revision": "9b746ed36ab0509a82cd",
+    "url": "/ekointel/static/js/2.5ae87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ekointel/static/js/2.5ae87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac2f1189d13f3f5c12d",
+    "url": "/ekointel/static/js/main.f02def67.chunk.js"
+  },
+  {
+    "revision": "5c3c06be3a2e3cc4dcf2",
+    "url": "/ekointel/static/js/runtime-main.3588553b.js"
+  },
+  {
+    "revision": "208ddc187ca08a6e627a7f23aad594d5",
+    "url": "/ekointel/static/media/home2.208ddc18.jpg"
+  },
+  {
+    "revision": "12de0585a9a152d98eaeae3aeb3e435b",
+    "url": "/ekointel/static/media/home3.12de0585.jpg"
+  },
+  {
+    "revision": "166d243ed0483e0a35bd2875c2f6db8b",
+    "url": "/ekointel/static/media/home4.166d243e.jpg"
+  },
+  {
+    "revision": "80f12f4711dc8cb6f007f97c31e6180e",
+    "url": "/ekointel/static/media/logo3.80f12f47.png"
+  }
+]);
